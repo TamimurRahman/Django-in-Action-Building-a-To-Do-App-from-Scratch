472bd8b638c1f24aac80ff4bcda1da5c755fa97f
TamimurRahman <tamimr008@gmail.com>
2025-09-06 16:56:38 +0600

    works models.py create Task model and create some module in models.py

---
+++ todo/task/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Task(models.Model):
+    STATUS =[
+        ('pending','pending'),
+        ('compleated','complated'),
+    ]
+    CATEGORY = [
+        ('work','work'),
+        ('personal','personal'),
+        ('other','other'),
+    ]
+    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    due_time = models.TimeField()
+    status = models.CharField(max_length=100,choices=STATUS,default='pending')
+    category = models.CharField(max_length=100,choices=CATEGORY)
+    is_compleated = models.BooleanField(default=False)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} | {self.status}'
+
